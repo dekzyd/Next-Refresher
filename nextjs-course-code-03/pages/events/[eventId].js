04667e7b47b1292e3a54fa1e4da3dfe5cd4cbe73
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-03-30 21:54:07 +0100

    add individual event page

--- nextjs-course-code-03/pages/events/[eventId].js
+++ nextjs-course-code-03/pages/events/[eventId].js
@@ -1,13 +1,31 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventDetailPage() {
   const router = useRouter();
-  console.log(router.pathname);
 
-  console.log(router.query.eventId);
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found...</p>;
+  }
   return (
-    <div>
-      <h1>Event Page {router.query.eventId}</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
