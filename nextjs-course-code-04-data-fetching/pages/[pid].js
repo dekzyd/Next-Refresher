15020e681d505346016463bcefc1cfd10a5b83ce
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-04-06 14:06:46 +0100

    dynamic pre-rendering

---
+++ nextjs-course-code-04-data-fetching/pages/[pid].js
@@ -0,0 +1,32 @@
+import { fragment } from "react";
+import fs from "fs/promises";
+import path from "path";
+
+function productDetailPage(props) {
+  const { loadedProduct } = props;
+  return (
+    <fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  // load dummy-backend.json data without fetching on CS
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export default productDetailPage;
--- nextjs-course-code-04-data-fetching/pages/index.js
+++ nextjs-course-code-04-data-fetching/pages/index.js
@@ -1,26 +1,46 @@
 import fs from "fs/promises";
 import path from "path";
+import { redirect } from "next/dist/server/api-utils";
+import Link from "next/link";
 
 function HomePage(props) {
   const { product } = props;
   return (
     <ul>
       {product.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  console.log("Regenerating");
   // load dummy-backend.json data without fetching on CS
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
       product: data.products,
     },
+    revalidate: 10,
   };
 }
 
